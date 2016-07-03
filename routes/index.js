07604a5d50eefa4d6b51287e9c3bafc9a4d3eeaf
ewadkins <ewadkins83@gmail.com>
2016-07-02 20:18:56 -0400

    Changed path to index

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var config = require('../config');
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 module.exports = function(db, logger){
 
 	router.get('/', function(req, res, next) {
-		res.redirect(config.server.index);
+		res.sendFile(path.join(config.server.appDirectory, config.server.publicDirectory, config.server.index));
 	});
 	
 	return router;
